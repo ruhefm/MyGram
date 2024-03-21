abdbb46c43b8eabbeb7f943248f72085f0435977
ruhefm <herupurnama09@gmail.com>
2024-03-21 14:45:54 +0700

    init exec tadi untuk debug, fix foreign key

--- models/models.go
+++ models/models.go
@@ -14,13 +14,13 @@ type Users struct {
 	Username        string          `json:"username" valid:"alphanum,minstringlength(3)" gorm:"unique;type:varchar(50);not null; default:null"`
 	Email           string          `json:"email" form:"email" valid:"email"  gorm:"unique;type:varchar(150);not null; default:null;uniqueIndex"`
 	Password        string          `json:"password" form:"password" valid:"minstringlength(6)" gorm:"type:text;not null; default:null"`
-	Age             uint            `json:"age" gorm: valid:"min:9" "type:int;not null; default:null"`
+	Age             uint            `json:"age" gorm:"not null;check:age >= 9"`
 	ProfileImageURL string          `json:"profile_image_url" valid:"url" gorm:"type:text"`
 	CreatedAt       time.Time       `json:"created_at" gorm:"type:timestamp"`
 	UpdatedAt       time.Time       `json:"updated_at" gorm:"type:timestamp"`
-	SocialMedias    []Social_Medias `json:"social_medias" gorm:"foreignkey:UserID; constraint:OnDelete:CASCADE"`
-	Photos          []Photos        `json:"photos" gorm:"foreignkey:UserID; constraint:OnDelete:CASCADE"`
-	Comments        []Comments      `json:"comments" gorm:"foreignkey:UserID; constraint:OnDelete:CASCADE"`
+	SocialMedias    []Social_Medias `json:"social_medias" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+	Photos          []Photos        `json:"photos" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+	Comments        []Comments      `json:"comments" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 }
 
 func (user *Users) BeforeCreate(tx *gorm.DB) (err error) {
@@ -36,16 +36,6 @@ func (user *Users) BeforeCreate(tx *gorm.DB) (err error) {
 	return nil
 }
 
-// Kurang berfungsi
-
-// func (user *Users) BeforeUpdate(tx *gorm.DB) (err error) {
-// 	if user.Password != "" {
-// 		hash := helpers.HashDong(user.Password)
-// 		user.Password = hash
-// 	}
-// 	return nil
-// }
-
 type LoginRequest struct {
 	Email    string `json:"email" valid:"required, email" binding:"required"`
 	Password string `json:"password" valid:"required,minstringlength(6)" binding:"required"`
@@ -56,11 +46,11 @@ type Photos struct {
 	Title     string     `json:"title" gorm:"type:varchar(100);not null; default:null"`
 	Caption   string     `json:"caption" gorm:"type:varchar(200);"`
 	PhotoURL  string     `json:"photo_url" valid:"url" gorm:"type:text;not null; default:null"`
-	UserID    uint       `json:"user_id" gorm:"type:bigint;not null; default:null"`
+	UserID    uint       `json:"user_id" gorm:"not null"`
 	User      Users      `json:"user" gorm:"foreignKey:UserID;"`
+	Comments  []Comments `json:"comments" gorm:"foreignKey:PhotoID;constraint:OnDelete:CASCADE"`
 	CreatedAt time.Time  `json:"created_at" gorm:"type:timestamp"`
 	UpdatedAt time.Time  `json:"updated_at" gorm:"type:timestamp"`
-	Comments  []Comments `json:"comments" gorm:"foreignkey:PhotoID"`
 }
 
 func (user *Photos) BeforeCreate(tx *gorm.DB) (err error) {
@@ -74,13 +64,13 @@ func (user *Photos) BeforeCreate(tx *gorm.DB) (err error) {
 
 type Comments struct {
 	ID        uint      `json:"id" gorm:"primary_key;type:bigint"`
-	UserID    uint      `json:"user_id" gorm:"type:bigint;not null; default:null"`
-	PhotoID   uint      `json:"photo_id" gorm:"type:bigint;not null; default:null"`
+	UserID    uint      `json:"user_id" gorm:"not null"`
+	PhotoID   uint      `json:"photo_id" gorm:"not null"`
 	Message   string    `json:"message" gorm:"type:varchar(200);"`
 	CreatedAt time.Time `json:"created_at" gorm:"type:timestamp"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"type:timestamp"`
-	User      Users     `json:"user" gorm:"foreignKey:UserID;"`
-	Photo     Photos    `json:"photo" gorm:"foreignKey:ID; "`
+	User      Users     `json:"user" gorm:"foreignKey:UserID"`
+	Photo     Photos    `json:"photo" gorm:"foreignKey:PhotoID"`
 }
 
 func (user *Comments) BeforeCreate(tx *gorm.DB) (err error) {
@@ -96,8 +86,8 @@ type Social_Medias struct {
 	ID             uint      `json:"id" gorm:"primary_key;type:bigint"`
 	Name           string    `json:"name" gorm:"type:varchar(50);not null; default:null"`
 	SocialMediaURL string    `json:"social_media_url" valid:"url" gorm:"type:varchar(50);not null; default:null"`
-	UserID         uint      `json:"user_id" gorm:"type:bigint;not null; default:null"`
-	User           Users     `json:"user" gorm:"foreignkey:UserID;" `
+	UserID         uint      `json:"user_id" gorm:"not null"`
+	User           Users     `json:"user" gorm:"foreignKey:UserID"`
 	CreatedAt      time.Time `json:"created_at" gorm:"type:timestamp"`
 	UpdatedAt      time.Time `json:"updated_at" gorm:"type:timestamp"`
 }
